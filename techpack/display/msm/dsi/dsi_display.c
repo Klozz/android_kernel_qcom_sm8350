f1751afb8fccb214fea3774e7e375dc36755f8ad
Sultan Alsawaf <sultan@kerneltoast.com>
2021-07-15 15:48:55 +0900

    adreno_tz: Fix GPU target frequency calculation for HFR
    It seems that the black-box GPU frequency selection algorithm is unaware
    of high refresh rates, and assumes a refresh rate of 60 Hz for its
    target frequencies. This results in frequencies that are too low being
    selected when a higher refresh rate, such as 90 Hz, is used.
    
    To remedy this, multiply the GPU's busy time by a ratio of the current
    refresh rate to 60 Hz. This makes msm-adreno-tz select the right
    frequencies needed to avoid stuttering.
    
    Change-Id: Id8b2f025ce9f74faa20ecbee42f27d69fd74f7de
    [ghostrider-reborn: support dfps]
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: klozz <carlosj@klozz.dev>

--- techpack/display/msm/dsi/dsi_display.c
+++ techpack/display/msm/dsi/dsi_display.c
@@ -68,11 +68,6 @@ bool is_skip_op_required(struct dsi_display *display)
 
 static unsigned int cur_refresh_rate = 60;
 
-unsigned int dsi_panel_get_refresh_rate(void)
-{
-	return READ_ONCE(cur_refresh_rate);
-}
-
 static inline void dsi_display_set_refresh_rate(unsigned int refresh_rate)
 {
 	if (READ_ONCE(cur_refresh_rate) == refresh_rate)
@@ -7510,7 +7505,10 @@ int dsi_display_validate_mode_change(struct dsi_display *display,
 					cur_mode->timing.v_front_porch,
 					adj_mode->timing.v_front_porch);
 			}
-			dsi_display_set_refresh_rate(adj_mode->timing.refresh_rate);
+			if (cur_mode->timing.refresh_rate != adj_mode->timing.refresh_rate) {
+				WRITE_ONCE(cur_refresh_rate, adj_mode->timing.refresh_rate);
+				DSI_DEBUG("cur_refresh_rate set to %d\n", adj_mode->timing.refresh_rate);
+			}
 		}
 
 		/* dynamic clk change use case */
@@ -8467,6 +8465,11 @@ static void dsi_display_panel_id_notification(struct dsi_display *display)
 	}
 }
 
+unsigned int dsi_panel_get_refresh_rate(void)
+{
+	return READ_ONCE(cur_refresh_rate);
+}
+
 int dsi_display_enable(struct dsi_display *display)
 {
 	int rc = 0;
@@ -8508,6 +8511,8 @@ int dsi_display_enable(struct dsi_display *display)
 	mutex_lock(&display->display_lock);
 
 	mode = display->panel->cur_mode;
+
+	WRITE_ONCE(cur_refresh_rate, mode->timing.refresh_rate);
 
 	if (mode->dsi_mode_flags & DSI_MODE_FLAG_DMS) {
 		rc = dsi_panel_post_switch(display->panel);
