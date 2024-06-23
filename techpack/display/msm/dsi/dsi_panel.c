1047e23a83b6bcc5886f0fced63258255b3f2225
klozz <carlosj@klozz.dev>
2024-06-23 02:57:00 +0000

    techpack&dts:some changes for M20 (FTS) panel
    Signed-off-by: klozz <carlosj@klozz.dev>

--- techpack/display/msm/dsi/dsi_panel.c
+++ techpack/display/msm/dsi/dsi_panel.c
@@ -387,13 +387,14 @@ static int dsi_panel_power_off(struct dsi_panel *panel)
 		DSI_DEBUG("TWM Enabled, skip panel power off\n");
 		return rc;
 	}
-	if (gpio_is_valid(panel->reset_config.disp_en_gpio))
-		gpio_set_value(panel->reset_config.disp_en_gpio, 0);
 
 	if (panel->mi_panel_id == 0x4D323000360200 
-     || panel->mi_panel_id == 0x4D323000420D00) {
+	     || panel->mi_panel_id == 0x4D323000420D00) {
 	  mdelay(5);
 	}
+
+	if (gpio_is_valid(panel->reset_config.disp_en_gpio))
+		gpio_set_value(panel->reset_config.disp_en_gpio, 0);
 
 	if (gpio_is_valid(panel->reset_config.reset_gpio) &&
 					!panel->reset_gpio_always_on)
