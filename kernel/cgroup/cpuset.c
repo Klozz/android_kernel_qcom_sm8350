31c226cf5d1e5ef6bf0915f28687d546880c6964
klozz <carlosj@klozz.dev>
2024-07-17 23:59:52 -0600

    cpuset:remove init.qcom.post from tampering
     Some new devices set up the cpuset by calling a shell script
     At this time, the comm captured by task_is_booster is "sh".
     Obviously this cannot be used as a marker.
    
    Signed-off-by: klozz <carlosj@klozz.dev>

--- kernel/cgroup/cpuset.c
+++ kernel/cgroup/cpuset.c
@@ -210,8 +210,7 @@ static inline bool task_is_booster(struct task_struct *tsk)
 	       !strcmp(comm, "perf@2.0-servic") ||
 	       !strcmp(comm, "perf@2.1-servic") ||
 	       !strcmp(comm, "perf@2.2-servic") ||
-	       !strcmp(comm, "power@2.0-servic") ||
-	       !strcmp(comm, "init.qcom.post_");
+	       !strcmp(comm, "power@2.0-servic");
 }
 
 static inline struct cpuset *css_cs(struct cgroup_subsys_state *css)
