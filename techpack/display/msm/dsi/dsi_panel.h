d533afd1e9a5e7fc8015a7af21a48378f8ae8951
klozz <carlosj@klozz.dev>
2024-07-30 19:36:05 -0600

    techpack: display add some esd checks
    Signed-off-by: klozz <carlosj@klozz.dev>

--- techpack/display/msm/dsi/dsi_panel.h
+++ techpack/display/msm/dsi/dsi_panel.h
@@ -169,6 +169,7 @@ enum esd_check_status_mode {
 
 struct drm_panel_esd_config {
 	bool esd_enabled;
+	bool esd_aod_enabled;
 
 	enum esd_check_status_mode status_mode;
 	struct dsi_panel_cmd_set status_cmd;
