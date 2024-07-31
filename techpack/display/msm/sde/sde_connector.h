ff2f7847b90928b5c76ce3bcf49892ee73243dea
klozz <carlosj@klozz.dev>
2024-07-30 19:36:05 -0600

    techpack: display add some esd checks
    Signed-off-by: klozz <carlosj@klozz.dev>

--- techpack/display/msm/sde/sde_connector.h
+++ techpack/display/msm/sde/sde_connector.h
@@ -513,6 +513,7 @@ struct sde_connector {
 	int lp_mode;
 	int last_panel_power_mode;
 	struct device *sysfs_dev;
+	int max_esd_check_power_mode;
 
 	struct msm_property_info property_info;
 	struct msm_property_data property_data[CONNECTOR_PROP_COUNT];
