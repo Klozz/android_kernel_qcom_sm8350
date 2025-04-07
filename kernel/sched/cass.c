32a20a442b4c45a6c98509791a540e4199fa57ce
klozz <carlosj@klozz.dev>
2025-04-06 23:44:16 -0600

    cass:fixes for 5.4
    Signed-off-by: klozz <carlosj@klozz.dev>

--- kernel/sched/cass.c
+++ kernel/sched/cass.c
@@ -147,8 +147,10 @@ static int cass_best_cpu(struct task_struct *p, int prev_cpu, bool sync, bool rt
 		struct rq *rq = cpu_rq(cpu);
 
 		/* Get the capacity of this CPU adjusted for thermal pressure */
-		curr->cap_max = arch_scale_cpu_capacity(cpu) -
+		/*curr->cap_max = arch_scale_cpu_capacity(cpu) -
 				thermal_load_avg(rq);
+		adjust for 5.4		*/
+		curr->cap_max = arch_scale_cpu_capacity(cpu) - thermal_cap(cpu);
 
 		/* Prefer the CPU that meets the uclamp minimum requirement */
 		if (curr->cap_max < uc_min && best->cap_max >= uc_min)
