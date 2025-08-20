7d73cc1e31860e6299f964d60a57ac4cc758a251
klozz <carlosj@klozz.dev>
2025-08-20 03:11:37 -0600

    cpuset:dont tamper perf2.2
    Signed-off-by: klozz <carlosj@klozz.dev>

--- kernel/cgroup/cpuset.c
+++ kernel/cgroup/cpuset.c
@@ -210,7 +210,6 @@ static inline bool task_is_booster(struct task_struct *tsk)
 	       !strcmp(comm, "perf@1.0-servic") ||
 	       !strcmp(comm, "perf@2.0-servic") ||
 	       !strcmp(comm, "perf@2.1-servic") ||
-	       !strcmp(comm, "perf@2.2-servic") ||
 	       !strcmp(comm, "power@2.0-servic");
 }
 
